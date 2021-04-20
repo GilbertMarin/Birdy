e3bd44024da0f6fabaf8dd687df22f375b39d62a
Shmaither M. Rizo <shmaither@gmail.com>
2021-04-20 22:19:38 +0000

    AG (feature): Fetch from API and show on the screen a bird name

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,12 +3,11 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { AudioGallery } from "./pages/audioGallery";
 
 //create your first component
 const Layout = () => {
@@ -25,13 +24,10 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="/audioGallery">
+							<AudioGallery />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
-						<Route>
+						<Route path="*">
 							<h1>Not found!</h1>
 						</Route>
 					</Switch>
