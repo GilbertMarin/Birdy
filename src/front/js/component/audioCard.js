fa94306e9f8fb5dcd1efa60a489dd69d891ac6ee
Shmaither M. Rizo <shmaither@gmail.com>
2021-04-22 02:38:43 +0000

    Add responsive grid with bird data

--- src/front/js/component/audioCard.js
+++ src/front/js/component/audioCard.js
@@ -1,24 +1,50 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import ReactAudioPlayer from "react-audio-player";
-import { Context } from "../store/appContext";
+import { PropTypes } from "prop-types";
+
+// React-Bootstrap
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-export const AudioCard = () => {
-	const { store, actions } = useContext(Context);
+export const AudioCard = ({ name, country, location, time, sound }) => {
+	// const { store, actions } = useContext(Context);
+	// <AudioCard key={index} name={bird.en} country={bird.cnt} location={bird.loc} time={bird.time} sound={store.birdSound[index]}/>;
 
 	return (
-		<div className="container">
-			<Card>
-				<Card.Header as="h5">{store.birdsRaw[0].en}</Card.Header>
-				<Card.Body>
-					<Card.Title>Subtitle about the bird</Card.Title>
-					<Card.Text>Facts about the bird.</Card.Text>
-					<ReactAudioPlayer src={store.birdSounds[0]} autoPlay controls />
-					<Button variant="primary">Add to favorite</Button>
-				</Card.Body>
-			</Card>
-		</div>
+		<Card>
+			<Card.Header as="h5">{name}</Card.Header>
+			<Card.Body>
+				<Card.Title>Subtitle about the bird</Card.Title>
+				<Card.Text>Country: {country}</Card.Text>
+				<Card.Text>Location: {location}</Card.Text>
+				<Card.Text>Time: {time}</Card.Text>
+				<ReactAudioPlayer src={sound} controls />
+				<Button variant="primary">Add to favorite</Button>
+			</Card.Body>
+		</Card>
 	);
 };
+
+AudioCard.propTypes = {
+	name: PropTypes.string,
+	country: PropTypes.string,
+	location: PropTypes.string,
+	time: PropTypes.string,
+	sound: PropTypes.string
+};
+
+//  <div class="container-fluid">
+//             <div class="row  mb-1">
+//               <div class="col px-0">
+//                 <img src="https://loremflickr.com/320/240?random=1" alt="">
+//               </div>
+//               <div class="col px-1">
+//                 <img src="https://loremflickr.com/320/240?random=2" alt="">
+//               </div>
+//               <div class="col px-0">
+//                 <img src="https://loremflickr.com/320/240?random=3" alt="">
+//               </div>
+//             </div>
--- src/front/js/pages/audioGallery.js
+++ src/front/js/pages/audioGallery.js
@@ -4,6 +4,9 @@ import ReactAudioPlayer from "react-audio-player";
 
 import { Context } from "../store/appContext";
 import { AudioCard } from "../component/audioCard";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export const AudioGallery = () => {
 	const { store, actions } = useContext(Context);
@@ -16,10 +19,25 @@ export const AudioGallery = () => {
 		<div className="container">
 			{!store.isPending ? (
 				<div>
-					{/* {store.people.map((character, index) => {
-						return <CharacterCard key={index} character={character} id={index} />;
-					})} */}
-					<AudioCard />
+					<Container>
+						<Row>
+							{store.birdsRaw.length == 0 ? (
+								<div>Loading...</div>
+							) : (
+								store.birdsRaw.map((bird, index) => (
+									<Col xs={12} md={6} lg={4} key={index}>
+										<AudioCard
+											name={bird.en}
+											country={bird.cnt}
+											location={bird.loc}
+											time={bird.time}
+											sound={store.birdSounds[index]}
+										/>
+									</Col>
+								))
+							)}
+						</Row>
+					</Container>
 				</div>
 			) : (
 				<h2>Loading. . .</h2>
