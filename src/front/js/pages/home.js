0c9ace03bec2922a1b914c416a7ab8b2c9078cf3
Shmaither M. Rizo <shmaither@gmail.com>
2021-04-28 04:33:01 +0000

    Add a navbar for internal views

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { NavbarBirdy } from "../component/navbar";
 
 import "../../styles/home.scss";
 
@@ -7,11 +8,14 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rodolfo!</h1>
-			<h1>Hello Rigo!</h1>
-			<h1>Lilliana Castro</h1>
-			<h1>Hello Shmaiter!</h1>
-		</div>
+		<>
+			<NavbarBirdy />
+			<div className="text-center mt-5">
+				<h1>Hello Rodolfo!</h1>
+				<h1>Hello Rigo!</h1>
+				<h1>Lilliana Castro</h1>
+				<h1>Hello Shmaiter!</h1>
+			</div>
+		</>
 	);
 };
--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -5,6 +5,9 @@ import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import { Sonnet } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
+import { NavbarBirdy } from "../component/navbar";
+
+import "../../styles/gif.scss";
 
 export const Profile = () => {
 	const { store, actions } = useContext(Context);
@@ -12,49 +15,48 @@ export const Profile = () => {
 		actions.getUser();
 	}, []);
 	return (
-		<div className="container bootstrap snippets bootdey">
-			{!store.isPending ? (
-				<div className="row">
-					<div className="profile-nav col-md-3">
-						<div className="panel">
-							<div className="user-heading round">
-								<a href="#">
-									<img
-										src="https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg"
-										alt=""
-										width="200px"
-										height="150px"
-									/>
-								</a>
-								<h1>{store.activeUser.first_name}</h1>
-								<h1>{store.activeUser.email}</h1>
-							</div>
+		<>
+			<NavbarBirdy />
+			<div className="container bootstrap snippets bootdey">
+				{!store.isPending ? (
+					<div className="row">
+						<div className="profile-nav col-md-3">
+							<div className="panel">
+								<div className="user-heading round">
+									<h1>{store.activeUser.first_name}</h1>
+									<h1>{store.activeUser.email}</h1>
+								</div>
 
-							<ul className="nav nav-pills nav-stacked">
-								<li>
-									<a href="#">
-										{" "}
-										<i className="fa fa-edit" /> Edit profile
-									</a>
-								</li>
-							</ul>
+								<ul className="nav nav-pills nav-stacked">
+									<li>
+										<a href="#">
+											{" "}
+											<i className="fa fa-edit" /> Edit profile
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div className="profile-info col-md-9">
+							<Tabs defaultActiveKey="Personal Information" id="uncontrolled-tab-example">
+								<Tab eventKey="Personal Information" title="Personal Information" />
+
+								<Tab eventKey="Favorites" title="Favorites" />
+								<h2 />
+								<Tab eventKey="Bitácora" title="Bitácora" />
+							</Tabs>
 						</div>
 					</div>
-					<div className="profile-info col-md-9">
-						<Tabs defaultActiveKey="Personal Information" id="uncontrolled-tab-example">
-							<Tab eventKey="Personal Information" title="Personal Information" />
-
-							<Tab eventKey="Favorites" title="Favorites" />
-							<h2 />
-							<Tab eventKey="Bitácora" title="Bitácora" />
-						</Tabs>
+				) : (
+					<div>
+						<Image
+							className="w-25"
+							src="https://media.giphy.com/media/3o7aCWH0iwyew3cLwQ/giphy.gif"
+							roundedCircle
+						/>
 					</div>
-				</div>
-			) : (
-				<div>
-					<Image src="https://media.giphy.com/media/xTcnSMB8VXjTox23zW/giphy.gif" roundedCircle />
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</>
 	);
 };
